2eb3cf12d5b889690e1a71ec2ed3cbf0c4880f46
mike <mikelikespie@gmail.com>
2009-03-29 00:23:19 -0400

    Refactored ConcurrentDataSet to get rid of stuff from header

--- lib/ConcurrentDataSet.cpp
+++ lib/ConcurrentDataSet.cpp
@@ -8,14 +8,86 @@
 #include "ConcurrentDataSet.h"
 
 /*
-template <> void *ConcurrentDataSet<const char *>::startRoutine(void *me)
+template ConcurrentDataSet::<> void *ConcurrentDataSet<const char *>::startRoutine(void *me)
 {
 	((ConcurrentDataSet*)me)->loop();
 	return NULL;
 }
 */
-void *ConcurrentDataSet::startRoutine(void *me)
+void ConcurrentDataSet::*ConcurrentDataSet::startRoutine(void *me)
 {
 	((ConcurrentDataSet*)me)->loop();
 	return NULL;
+}
+
+ConcurrentDataSet() {
+	pthread_mutex_init(&mutex_run, NULL);
+	pthread_cond_init(&cond_run, NULL);
+	finished = false;
+	exit = false;
+	startRun = false;
+
+}
+
+virtual ConcurrentDataSet::~ConcurrentDataSet() {
+	//Signal for us to exit
+	pthread_mutex_lock(&mutex_run);
+	exit = true;
+	pthread_mutex_unlock(&mutex_run);
+	pthread_cond_signal(&cond_run);
+}
+
+void ConcurrentDataSet::startThread() {
+	pthread_create(&thread, NULL, startRoutine, (void*) this);
+}
+
+void ConcurrentDataSet::startGetTopKSimilar(ObjectValueMap *actorMap, int k, DistanceFunction df) {
+	functionParams.actorMap = actorMap;
+	functionParams.df = df;
+	functionParams.k = k;
+
+
+	pthread_mutex_lock(&mutex_run);
+	startRun=true;
+	pthread_mutex_unlock(&mutex_run);
+	pthread_cond_signal(&cond_run);
+
+}
+
+FloatKeyVec ConcurrentDataSet::getTopKSimilar() {
+	//Now we have to wait for the mutex to unlock
+	pthread_mutex_lock(&mutex_run);
+	while (!finished) {
+		pthread_cond_wait(&cond_run, &mutex_run);
+	}
+	finished = false;
+	pthread_mutex_unlock(&mutex_run);
+
+	return functionParams.similaritiesRet;
+}
+
+
+inline ConcurrentDataSet::void loop() {
+	while (true) {
+		//Now we have to wait for the mutex to unlock
+		pthread_mutex_lock(&mutex_run);
+		while (!startRun) {
+			pthread_cond_wait(&cond_run, &mutex_run);
+		}
+		startRun = false;
+
+		if (exit) {
+			pthread_mutex_unlock(&mutex_run);
+			pthread_exit(NULL);
+		}
+		pthread_mutex_unlock(&mutex_run);
+
+		functionParams.similaritiesRet = DataSet::getTopKSimilar(functionParams.actorMap,
+						functionParams.k, functionParams.df);
+
+		pthread_mutex_lock(&mutex_run);
+		finished = true;
+		pthread_mutex_unlock(&mutex_run);
+		pthread_cond_signal(&cond_run);
+	}
 }
--- lib/ConcurrentDataSet.h
+++ lib/ConcurrentDataSet.h
@@ -20,14 +20,14 @@
  */
 
 using namespace std; // THIS IS BAD< but I am lazy
-class ConcurrentDataSet: public DataSet {
+class ConcurrentDataSet: public DataSet;
 
 private:
 	pthread_t thread;
 	pthread_mutex_t mutex_run;
 	pthread_cond_t cond_run;
 
-	struct {
+	struct;
 		ObjectValueMap *actorMap;
 		DistanceFunction df;
 		int k; //-1 = return all
@@ -45,78 +45,17 @@ private:
 	static void *startRoutine(void *me);
 
 public:
-	ConcurrentDataSet() {
-		pthread_mutex_init(&mutex_run, NULL);
-		pthread_cond_init(&cond_run, NULL);
-		finished = false;
-		exit = false;
-		startRun = false;
+	ConcurrentDataSet();
 
-	}
+	virtual ~ConcurrentDataSet();
 
-	virtual ~ConcurrentDataSet() {
-		//Signal for us to exit
-		pthread_mutex_lock(&mutex_run);
-		exit = true;
-		pthread_mutex_unlock(&mutex_run);
-		pthread_cond_signal(&cond_run);
-	}
+	void startThread();
 
-	void startThread() {
-		pthread_create(&thread, NULL, startRoutine, (void*) this);
-	}
+	void startGetTopKSimilar(ObjectValueMap *actorMap, int k, DistanceFunction df);
 
-	void startGetTopKSimilar(ObjectValueMap *actorMap, int k, DistanceFunction df) {
-		functionParams.actorMap = actorMap;
-		functionParams.df = df;
-		functionParams.k = k;
-
-
-		pthread_mutex_lock(&mutex_run);
-		startRun=true;
-		pthread_mutex_unlock(&mutex_run);
-		pthread_cond_signal(&cond_run);
-
-	}
-
-	FloatKeyVec getTopKSimilar() {
-		//Now we have to wait for the mutex to unlock
-		pthread_mutex_lock(&mutex_run);
-		while (!finished) {
-			pthread_cond_wait(&cond_run, &mutex_run);
-		}
-		finished = false;
-		pthread_mutex_unlock(&mutex_run);
-
-		return functionParams.similaritiesRet;
-	}
-
-
+	FloatKeyVec getTopKSimilar();
 private:
-	inline void loop() {
-		while (true) {
-			//Now we have to wait for the mutex to unlock
-			pthread_mutex_lock(&mutex_run);
-			while (!startRun) {
-				pthread_cond_wait(&cond_run, &mutex_run);
-			}
-			startRun = false;
-
-			if (exit) {
-				pthread_mutex_unlock(&mutex_run);
-				pthread_exit(NULL);
-			}
-			pthread_mutex_unlock(&mutex_run);
-
-			functionParams.similaritiesRet = DataSet::getTopKSimilar(functionParams.actorMap,
-							functionParams.k, functionParams.df);
-
-			pthread_mutex_lock(&mutex_run);
-			finished = true;
-			pthread_mutex_unlock(&mutex_run);
-			pthread_cond_signal(&cond_run);
-		}
-	}
+	void loop();
 };
 
 #endif /* CONCURRENTDATASET_H_ */
