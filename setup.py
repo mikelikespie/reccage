4d4dc97d611c873bc4384e1fc8f77d08dbd5c114
Mike Lewis <mlewis@scribd.com>
2010-02-05 16:30:24 -0800

    Changed setup.py to allow for build to happen without cython (if pDataSet.cpp is included)

--- setup.py
+++ setup.py
@@ -1,17 +1,30 @@
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
+
+
+cython_files = ['src/pDataSet.pyx']
+alternative_cpp_files = ['src/pDataSet.cpp']
+
+regular_cpp_files = [
+    "src/DataSet.cpp",
+    "src/DistanceFunctions.cpp",
+    "src/ConcurrentDataSet.cpp",
+    "src/ConcurrentDataSetPool.cpp"
+]
+
+wrapper_files = cython_files
+
+try:
+    from Cython.Distutils import build_ext
+except ImportError:
+    from distutils.command.build_ext import build_ext
+    wrapper_files = alternative_cpp_files
+
 
 ext_modules = [
     Extension(
         "reccage",                 # name of extension
-        [
-            "src/pDataSet.pyx",
-            "src/DataSet.cpp",
-            "src/DistanceFunctions.cpp",
-            "src/ConcurrentDataSet.cpp",
-            "src/ConcurrentDataSetPool.cpp"
-        ],
+        regular_cpp_files + alternative_cpp_files,
         # filename of our Cython source
         language="c++",              # this causes Cython to create C++ source
         include_dirs=['src'],          # usual stuff
